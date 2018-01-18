5eb05cccefe58f384e6b65e84d9d903d3b597490
tochman <thomas@craftacademy.se>
2018-01-18 16:46:03 +0100

    adds specs for #players

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,10 +1,14 @@
 class RPS
+  attr_accessor :players
   attr_reader :choices
 
   POSSIBLE_CHOICES = ['rock', 'paper', 'scissors']
+  DEFAULT_PLAYER_NAMES = ['Player 1', 'Player 2']
 
-  def initialize
+  def initialize(args = {})
     @choices = POSSIBLE_CHOICES
+    @players = args[:players] || DEFAULT_PLAYER_NAMES
   end
+
 
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -12,5 +12,30 @@ describe RPS do
     expect(subject.choices).to eq ['rock', 'paper', 'scissors']
   end
 
+  it { is_expected.to respond_to :players}
+
+  it '#players returns an array' do
+    expect(subject.players.class).to eq Array
+  end
+
+  it 'needs to have at least 2 players' do
+    expect(subject.players.size).to be >= 2
+  end
+
+  it 'has default playrs stored on initialize' do
+    expect(subject.players.first).to eq 'Player 1'
+  end
+
+  it 'accepts custom player names' do
+    subject.players = ['Kalle', 'Anders']
+    expect(subject.players.first).to eq 'Kalle'
+  end
+
+  it 'accepts custom player names on initialize' do
+    new_game = RPS.new(players: ['Kalle', 'Anders'])
+    expect(new_game.players.first).to eq 'Kalle'
+  end
+
+
 
 end
